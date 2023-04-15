fe63e3251ee852e89094b93a54b017cf9b1976e7
“[x22136851]” <x22136851@student.ncirl.ie>
2023-04-15 06:08:08 +0100

    sudo nginx -t at nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -7,8 +7,8 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp /home/ubuntu/CDOS/nginx/nginx.conf /etc/nginx/sites-available/onlinequiz
 sudo ln -s /etc/nginx/sites-available/onlinequiz /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-#sudo nginx -t
+sudo nginx -t
 sudo usermod -a -G www-data ubuntu
-
+sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
