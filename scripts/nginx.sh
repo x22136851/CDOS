0b8b222694d0b3800d8ca1d99cf3f570b7a87240
“[x22136851]” <x22136851@student.ncirl.ie>
2023-04-15 19:23:47 +0100

    changed nginx

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,18 +1,22 @@
 
-#!/usr/bin/bash
+# #!/usr/bin/bash
 
+# sudo systemctl daemon-reload
+# sudo rm -f /etc/nginx/sites-enabled/default
+
+# sudo cp /home/ubuntu/CDOS/nginx/nginx.conf /etc/nginx/sites-available/onlinequiz
+# #sudo ln -s /etc/nginx/sites-available/onlinequiz /etc/nginx/sites-enabled/
+# #sudo systemctl reload nginx
+# sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+# sudo nginx -t
+# sudo usermod -a -G www-data ubuntu
+# sudo nginx -t
+# sudo gpasswd -a www-data ubuntu
+# sudo systemctl restart nginx
+
+##########################################################update#################
 sudo systemctl daemon-reload
-sudo rm -f /etc/nginx/sites-enabled/default
-
-sudo cp /home/ubuntu/CDOS/nginx/nginx.conf /etc/nginx/sites-available/onlinequiz
-#sudo ln -s /etc/nginx/sites-available/onlinequiz /etc/nginx/sites-enabled/
-#sudo systemctl reload nginx
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo systemctl restart gunicorn
+sudo cp ngnix/ngnix.config /etc/ngnix/conf.d/ngnix.config
 sudo nginx -t
-sudo usermod -a -G www-data ubuntu
-sudo nginx -t
-sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
-
-
-
