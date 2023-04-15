f0540f61a15eae899047a6a92eedc352b1ea102a
“[x22136851]” <x22136851@student.ncirl.ie>
2023-04-15 05:35:08 +0100

    changed nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/CDOS/nginx/nginx.conf /etc/nginx/sites-available/CDOS/onlinequiz-master/onlinequiz
-sudo ln -s /etc/nginx/sites-available/CDOS/onlinequiz-master/onlinequiz /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/CDOS/nginx/nginx.conf /etc/nginx/sites-available/onlinequiz
+sudo ln -s /etc/nginx/sites-available/onlinequiz /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
