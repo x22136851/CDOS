8bc279d003f85a7945f7a9496b091a822c4db0c6
“[x22136851]” <x22136851@student.ncirl.ie>
2023-04-16 21:38:05 +0100

    afterinstall

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,20 @@
-#!/usr/bin/bash
+# #!/usr/bin/bash
 
-echo "Pull Finished"
-sudo systemctl daemon-reload
-sudo systemctl restart nginx
+# echo "Pull Finished"
+# sudo systemctl daemon-reload
+# sudo systemctl restart nginx
+
+
+
+##########changes#########
+
+#!/bin/bash
+# Create a virtual environment and activate it
+virtualenv venv
+source venv/bin/activate
+
+# Install the Flask application's dependencies
+
+
+# Start the Flask application
+nohup python3 /var/www/html/python3 manage.py runserver 0.0.0.0:8000.py > /dev/null 2>&1 &
